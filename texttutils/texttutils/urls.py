9a64c91434dd1dad91fa5639347c18c7572d709f
suraj kumar agarwal <surajkumaragarwal36@gmail.com>
2021-09-07 15:39:41 +0530

    version 2 update commit

--- texttutils/texttutils/urls.py
+++ texttutils/texttutils/urls.py
@@ -17,8 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('',views.index,name='index'),
+#     path('about/',views.about,name='about')
+# ]
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
-    path('about/',views.about,name='about')
+    path('analyze',views.analyze,name='analyze'),
+
 ]
--- texttutils/texttutils/views.py
+++ texttutils/texttutils/views.py
@@ -1,9 +1,51 @@
 #i have added this file
 from django.http import HttpResponse
-def index(request):
-    return HttpResponse('''<h1>Suraj</h1> <li> <a href="https://www.udemy.com/course/git-github-practical-guide/learn/lecture/28082510#overview">UDEmy Github</a></li>
-    <li> <a href="https://leetcode.com/problemset/all/">Leetcode</a></li>
-    <li> <a href="https://www.geeksforgeeks.org/must-do-coding-questions-for-companies-like-amazon-microsoft-adobe/">GFG must do</a></li>
-''')
-def about(request):
-    return HttpResponse("<h1> about hello </h1>")
+from django.shortcuts import render
+def  index(request):
+    params={'name':'suraj','place':'Earth'}
+    return render(request,'index.html',params)
+
+def analyze(request):
+    # get the text here
+    djtext=request.POST.get('text','default')
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    newlineremover = request.POST.get('newlineremover','off')
+    extraspaceremover = request.POST.get('extraspaceremover','off')
+
+    if removepunc == "on":
+        analyzed = ""
+        punctuations = '''!()-[}]{;:'"\/,.<>?@#$%^&*_~+='''
+        for char in djtext:
+            if char not in punctuations:
+                analyzed=analyzed+char
+        params={'purpose':'Remove Punctuation','analyzed_text': analyzed}
+        # analyze the text
+        djtext=analyzed
+        # return render(request, 'analyze.html',params)
+    if (fullcaps == "on"):
+        analyzed=""
+        for char in djtext:
+            analyzed=analyzed+char.upper()
+        params = {'purpose': 'Change to Uppercase', 'analyzed_text': analyzed}
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
+    if (newlineremover=="on"):
+        analyzed=""
+        for char in djtext:
+            if char !="\n" and char !="\r":
+                analyzed = analyzed + char
+        params = {'purpose': 'Remove new lines ', 'analyzed_text': analyzed}
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
+    if (extraspaceremover=="on"):
+        analyzed=""
+        for index,char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1]==" "):
+                analyzed = analyzed + char
+        params = {'purpose': 'Remove Extra Space  ', 'analyzed_text': analyzed}
+    if(removepunc !="on" and extraspaceremover !="on" and newlineremover !="on" and fullcaps!="on") :
+        return HttpResponse("Please chosse any of these 4 options to do some operaton ")
+    
+
+    return render(request,'analyze.html',params)
